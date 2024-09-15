312b84c04cc98616ca9f50999748d1d9a308bd26
Randy Fisher <rgftbc@yahoo.com>
2024-09-15 16:28:02 -0500

    commit

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
-const standardFieldingPath = '2018-standard-fielding.csv'
-const standardBattingPath = '2018-standard-batting.csv'
-const battingPitchingPath = '2018-batting-pitching.csv'
-const standardPitchingPath = '2018-standard-pitching.csv'
+const standardFieldingPath = '1994-standard-fielding.csv'
+const standardBattingPath = '1994-standard-batting.csv'
+const battingPitchingPath = '1994-batting-pitching.csv'
+const standardPitchingPath = '1994-standard-pitching.csv'
 
 module.exports = {
     standardBattingPath: standardBattingPath,
